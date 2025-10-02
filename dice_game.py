64d1d74dfa05e867f510e6951d3d57ffe5496ef4
yabisoghomonyan <yabisoghomonyan@gmail.com>
2025-10-02 18:57:53 +0400

    dice_game.py

--- dice_game.py
+++ dice_game.py
@@ -2,30 +2,24 @@ import random
 def trow():
     dice1=random.randint(1,6)
     dice2=random.randint(1,6)
-    return dice1,dice2
+    goal=dice1+dice2
+    return dice1,dice2,goal
 def result():
-    dice_1,dice_2=trow()
-    goal=dice_1+dice_2
-    if goal==11 or goal==7:
-        print(f"The sum of dice is {dice_1} + {dice_2} = {goal}")
+    dice_1,dice_2,goal=trow()
+    print(f"The sum of dice is {dice_1} + {dice_2} = {goal}")
+    if goal in (7, 11):
         print("You won")
-    elif goal==12 or goal==2 or goal==3:
-        print(f"The sum of dice is {dice_1} + {dice_2} = {goal}")
+    elif goal in (2,3,12):
         print("You lose")
     else:
-        print(f"The sum of dice is {dice_1} + {dice_2} = {goal}")
         print(f"Now your goal number is {goal}")
         while True:
-            dice_1,dice_2=trow()
-            goal1=dice_1+dice_2
+            dice_1,dice_2,goal1=trow()
+            print(f"The sum of dice is {dice_1} + {dice_2} = {goal}")
             if goal1==7:
-                print(f"The sum of dice is {dice_1} + {dice_2} = 7")
                 print("You lose")
                 break
             elif goal1==goal:
-                print(f"The sum of dice is {dice_1} + {dice_2} = {goal}")
-                print(" You won")
+                print("You won")
                 break
-            else:
-                print(f"The sum of dice is {dice_1} + {dice_2} = {goal}")
 result()
