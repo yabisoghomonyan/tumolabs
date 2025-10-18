78f3dd3eca7c19cd9bcafc9ab9029d0967c0b170
yabisoghomonyan <yabisoghomonyan@gmail.com>
2025-10-18 19:03:34 +0400

    timer.py

--- project.py
+++ project.py
@@ -1,8 +1,21 @@
 import time
-hour=int(input("enter the hour: "))
-minute=int(input("enter the minute: "))
-second=int(input("enter the second: "))
-def timer(hour,minute,second):
+def for_input():
+    while True:
+        try:
+            time_str = input("Enter time in h:m:s format: ")
+            h, m, s = map(int, time_str.split(':'))
+            print(f"Hours: {h}, Minutes: {m}, Seconds: {s}")
+            if h < 0 or m < 0 or s < 0:
+                raise  ValueError
+        except ValueError:
+            print("Invalid input: all parts must be numbers.")
+        else:
+            break
+    return h,m,s
+def timer(h,m,s):
+    hour=h
+    minute=m
+    second=s
     sec=hour*3600+minute*60+second
     while sec!=0:
         print(f"{hour:02d}:{minute:02d}:{second:02d}")
@@ -13,5 +26,5 @@ def timer(hour,minute,second):
         minute=(sec%3600)//60
     print(f"{hour:02d}:{minute:02d}:{second:02d}")
     print("Time is up")
-
-timer(hour,minute,second)
+h,m,s=for_input()
+timer(h,m,s)
